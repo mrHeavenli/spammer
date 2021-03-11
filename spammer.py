1031ddd5621a5209e12a7bf5f1493bcffee9a9a0
mrHeavenli <71031776+mrHeavenli@users.noreply.github.com>
2021-03-11 11:28:58 +0100

    deobfuscate
    The code was pretty abhorrent, so i cleaned it up in like 5 mins

--- spammer.py
+++ spammer.py
@@ -2,33 +2,48 @@ from time import sleep
 import pyautogui
 
 run = True
+
 while run:
     print('1 Autoclicker (for sticker)')
     print('2 Typewriter (for text)')
     print('3 Type E to quit')
-    o = input('')
-    if o == '1':
-        x = 0
-        o = int(input('How many clicks?'))
+    
+    option = input('')
+    
+    if option == '1':
+        i = 0
+        option = int(input('How many clicks?'))
+        
         print("You have 6 seconds before the spam starts")
         sleep(6)
-        while x != o:
-            x += 1
+        
+        while i != option:
+            i += 1
             sleep(1)
+            
             pyautogui.leftClick()
+        
         print('Done!')
-    elif o == '2':
-        x = 0
+    
+    elif option == '2':
+        i = 0
+        
         num = int(input('Number of messages to send:'))
-        nachricht = input('Content of messages:')
+        msg = input('Content of messages:')
+        
         print("You have 6 seconds before the spam starts")
         sleep(6)
-        while x < num:
-            x += 1
-            pyautogui.typewrite(nachricht)
+        
+        while i < num:
+            i += 1
+            
+            pyautogui.typewrite(msg)
             pyautogui.press('enter')
+        
         print('Done!')
-    elif o == 'E':
+    
+    elif option == 'E':
         quit()
+    
     else:
         pass
